da44f93fc30a31a425f0df06cb0c09e6bbc9e8c4
ANewmy <andrew@phosphorus.io>
2018-03-28 10:04:38 -0700

    Feedback/map

--- app/components/Feedback.js
+++ app/components/Feedback.js
@@ -1,65 +1,221 @@
 // @flow
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	Picker,
+	TouchableOpacity
+} from 'react-native';
+import StarRating from 'react-native-star-rating';
 
 class Feedback extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
 
-    submit() {
-        console.log('Submit clicked.. fetching data');
+		this.state = {
+			comments: '',
+			rating: 3,
+			ratingText: 'Average'
+		};
+	}
+	onStarRatingPress(rating) {
+		console.log('rating: ', rating);
+		this.setState({
+			rating: rating
+		});
+		switch (rating) {
+			case 1:
+				this.setState({
+					ratingText: 'Terrible'
+				});
+				break;
+			case 2:
+				this.setState({
+					ratingText: 'Poor'
+				});
+				break;
+			case 3:
+				this.setState({
+					ratingText: 'Average'
+				});
+				break;
+			case 4:
+				this.setState({
+					ratingText: 'Great'
+				});
+				break;
+			case 5:
+				this.setState({
+					ratingText: 'Excelent'
+				});
+				break;
+			default:
+				break;
+		}
+	}
 
-        fetch('http://127.0.0.1:8086/listUsers')
-            .then(response => {
-                if (response.status != 200) {
-                    console.log('resposnse: ', response);
-                }
-                return response.json();
-            })
-            .then(responseJson => {
-                console.log('responseJSON: ', responseJson);
-                console.log('His name is: ', responseJson.name);
-                console.log('his password is: ', responseJson.password);
-            })
-            .catch(error => {
-                console.log('error: ', error);
-            });
-    }
+	submit() {
+		//const url = 'http://192.168.1.79:8000/polls/feedback/';
+		const url = 'http://127.0.0.1:8000/polls/feedback/';
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity onPress={() => this.submit()} style={styles.submit}>
-                    <Text style={styles.submitText}>Submit</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+		var fetchBody = JSON.stringify({
+			timestamp: '2012-09-04 06:00:00',
+			satisfactory_level: this.state.rating,
+			comments: this.state.comments
+		});
+
+		console.log('Submit clicked.. fetching data: ', url, fetchBody);
+
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: fetchBody
+		})
+			.then(response => {
+				console.log('resposnse: ', response);
+
+				return response.json();
+			})
+			.then(responseJson => {
+				console.log('responseJSON: ', responseJson);
+			})
+			.catch(error => {
+				console.log('error: ', error);
+			});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.contactView}>
+					<View style={styles.titleView}>
+						<Text style={styles.titleText}>Contact Us</Text>
+					</View>
+				</View>
+
+				<View style={styles.inputView}>
+					<View style={styles.titleView}>
+						<Text style={styles.titleText}>Feedback</Text>
+					</View>
+					<View style={styles.starView}>
+						<Text
+							style={{
+								fontSize: 13,
+								marginTop: 5,
+								alignSelf: 'center',
+								fontFamily: 'Avenir-Roman',
+								color: 'black'
+							}}
+						>
+							{this.state.ratingText}
+						</Text>
+						<StarRating
+							disabled={false}
+							fullStarColor="gold"
+							emptyStarColor="black"
+							starSize={25}
+							buttonStyle={{ padding: 5 }}
+							starPadding={10}
+							maxStars={5}
+							rating={this.state.rating}
+							selectedStar={rating => this.onStarRatingPress(rating)}
+						/>
+					</View>
+
+					<TextInput
+						style={styles.commentInput}
+						multiline={true}
+						numberOfLines={6}
+						autoCapitalize="none"
+						placeholder="Leave your comments here"
+						onChangeText={comments => this.setState({ comments: comments })}
+						placeholderTextColor="rgba(0,0,0,.5)"
+					/>
+
+					<View style={styles.submitView}>
+						<View style={{ flex: 1 }} />
+						<TouchableOpacity
+							style={styles.submit}
+							onPress={() => this.submit()}
+						>
+							<Text style={styles.submitText}>Submit</Text>
+						</TouchableOpacity>
+						<View style={{ flex: 1 }} />
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    submit: {
-        //flex: 1,
-        height: 60,
-        width: 150,
-        borderRadius: 6,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'blue'
-    },
-    submitText: {
-        fontSize: 18,
-        color: 'white',
-        fontFamily: 'Avenir-Heavy'
-    }
+	container: {
+		flex: 10,
+		flexDirection: 'column'
+	},
+	submit: {
+		flex: 1,
+		margin: 12,
+		borderRadius: 6,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'blue'
+	},
+	submitText: {
+		fontSize: 14,
+		color: 'white',
+		fontFamily: 'Avenir-Heavy'
+	},
+	starView: {
+		flex: 0.8,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	contactView: {
+		margin: 15,
+		flex: 4,
+		backgroundColor: 'rgba(255,255,255,.7)',
+		borderColor: 'grey',
+		borderRadius: 5,
+		borderWidth: StyleSheet.hairlineWidth
+	},
+	inputView: {
+		margin: 15,
+		marginTop: 0,
+		flex: 6,
+		backgroundColor: 'rgba(255,255,255,.7)',
+		borderColor: 'grey',
+		borderRadius: 5,
+		borderWidth: StyleSheet.hairlineWidth
+	},
+	commentInput: {
+		flex: 1,
+		marginRight: 15,
+		marginLeft: 15
+	},
+	submitView: {
+		flex: 1,
+		flexDirection: 'row'
+	},
+	titleView: {
+		flex: 0.8,
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start'
+	},
+	titleText: {
+		flex: 1,
+		marginLeft: 15,
+		marginTop: 5,
+		fontSize: 18,
+		fontFamily: 'Avenir-Heavy',
+		color: 'black'
+	}
 });
 
 export default Feedback;
--- app/components/Map.js
+++ app/components/Map.js
@@ -6,51 +6,56 @@ import MapView from 'react-native-maps';
 import { connect } from 'react-redux';
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <MapView
-                    style={styles.map}
-                    region={{
-                        latitude: this.props.coords.latitude,
-                        longitude: this.props.coords.longitude,
-                        latitudeDelta: this.props.deltas.latitudeDelta,
-                        longitudeDelta: this.props.deltas.longitudeDelta
-                    }}
-                >
-                    <MapView.Marker coordinate={this.props.coords} title={'title'} description={'description'} />
-                </MapView>
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View style={styles.container}>
+				<MapView
+					style={styles.map}
+					mapType="satellite"
+					region={{
+						latitude: this.props.coords.latitude,
+						longitude: this.props.coords.longitude,
+						latitudeDelta: this.props.deltas.latitudeDelta,
+						longitudeDelta: this.props.deltas.longitudeDelta
+					}}
+				>
+					<MapView.Marker
+						coordinate={this.props.coords}
+						title={'title'}
+						description={'description'}
+					/>
+				</MapView>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    map: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
-    }
+	container: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: 'flex-end',
+		alignItems: 'center'
+	},
+	map: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0
+	}
 });
 
 const mapStateToProps = (state, props) => ({
-    coords: props.coords,
-    deltas: props.deltas
+	coords: props.coords,
+	deltas: props.deltas
 });
 
 export default connect(mapStateToProps)(Map);
